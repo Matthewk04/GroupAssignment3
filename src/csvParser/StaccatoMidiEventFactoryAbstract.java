02384fee2a3f4471a7caa78599fdbc013ccc4c38
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-04-03 10:52:22 -0500

    finished up the stacato midi event factories

--- src/csvParser/StaccatoMidiEventFactoryAbstract.java
+++ src/csvParser/StaccatoMidiEventFactoryAbstract.java
@@ -3,9 +3,27 @@
  */
 package csvParser;
 
+import javax.sound.midi.*;
+
 /**
- * 
+ * created to make the stacato events where notes are shorter and more detached
  */
 public class StaccatoMidiEventFactoryAbstract extends LegatoMidiEventFactoryAbstract {
+	 @Override
+	    public MidiEvent createLegatoEvent(int channel, int note, int velocity, long tick) 
+	            throws InvalidMidiDataException {
 
+	        ShortMessage noteOn = new ShortMessage();
+	        noteOn.setMessage(ShortMessage.NOTE_ON, channel, note, velocity);
+
+	        long noteDuration = 100; 
+	        
+	        ShortMessage noteOff = new ShortMessage();
+	        noteOff.setMessage(ShortMessage.NOTE_OFF, channel, note, 0);
+
+	        MidiEvent noteOnEvent = new MidiEvent(noteOn, tick);
+	        MidiEvent noteOffEvent = new MidiEvent(noteOff, tick + noteDuration);
+
+	        return noteOnEvent;
+	    }
 }
