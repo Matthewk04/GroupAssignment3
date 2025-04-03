b7d439bbc1a658e087446743c5d26139e19c2368
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-04-03 10:35:16 -0500

    impimented the pitch strategy to change the pitch up and down

---
+++ src/csvParser/PitchStrategy.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package csvParser;
+
+/**
+ * Interface for pitch modification strategies.
+ */
+public interface PitchStrategy {
+    int modifyPitch(int note);
+}
+
+class HigherPitchStrategy implements PitchStrategy {
+    @Override
+    public int modifyPitch(int note) {
+        return note + 2;
+    }
+}
+
+class LowerPitchStrategy implements PitchStrategy {
+    @Override
+    public int modifyPitch(int note) {
+        return note - 2;
+    }
+}
