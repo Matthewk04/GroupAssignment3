7a29a65dd9cfea8641286993bf91eee5d1da5dd1
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-04-03 10:43:13 -0500

    finished up legatomidieventfactoryabstract.java file

--- src/csvParser/LegatoMidiEventFactoryAbstract.java
+++ src/csvParser/LegatoMidiEventFactoryAbstract.java
@@ -3,9 +3,15 @@
  */
 package csvParser;
 
+import javax.sound.midi.*;
+
 /**
- * 
+ * abstract factory for creating legato events
+ * sublcasses impollement the methods to generate midi events
  */
 public class LegatoMidiEventFactoryAbstract {
-
+	public MidiEvent createLegatoEvent(int channel, int note, int velocity, long tick) 
+            throws InvalidMidiDataException {
+		return null;
+	}
 }
