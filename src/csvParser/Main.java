bc62570443776c43e79ac481d4303d46542cb507
aheg7b <157038626+aheg7b@users.noreply.github.com>
2025-04-03 10:35:49 -0500

    just pushing everything else

--- src/csvParser/Main.java
+++ src/csvParser/Main.java
@@ -1,4 +1,3 @@
-//just a test to make sure things are working
 package csvParser;
 
 import java.util.List;
@@ -15,49 +14,49 @@ public class Main {
 			}
 			
 			
-//			Sequence sequence = new Sequence ( Sequence.PPQ, 384 ) ;
-//			Track track = sequence.createTrack ( ) ;
+			Sequence sequence = new Sequence ( Sequence.PPQ, 384 ) ;
+			Track track = sequence.createTrack ( ) ;
 
-//			MidiEventFactoryAbstract factoryAbstract = new StandardMidiEventFactoryAbstract ( ) ;
-//			// MidiEventFactoryAbstractfactoryAbstract = new LegatoMidiEventFactoryAbstract ( ) ;
-//			// MidiEventFactoryAbstractfactoryAbstract = new StaccatoMidiEventFactoryAbstract ( ) ;
-//
-//			MidiEventFactory factory = factoryAbstract.createFactory ( ) ;
-//
-//			// Choose an instrument strategy ( e . g . , Trumpet , Bass Guitar , Piano )
-//			InstrumentStrategy instrumentStrategy = new ElectricBassGuitarStrategy ( ) ;
-//			instrumentStrategy.applyInstrument ( track , 0 ) ;
-//			instrumentStrategy = new TrumpetStrategy ( ) ;
-//			instrumentStrategy.applyInstrument (track , 1 ) ;
-//
-//			// Choose a pitch strategy ( e.g. , HigherPitch ,LowerPitch )
-//			PitchStrategy pitchStrategy = new HigherPitchStrategy ( );
-//			for (MidiEventData event:midiEvents ) {
-//				int modifiedNote = pitchStrategy.modifyPitch ( event.getNote ( ) ) ;
-//
-//				// call this as much as you want if you want to get a higher pitch
-//				modifiedNote = pitchStrategy.modifyPitch (modifiedNote ) ;
-//				if (event.getNoteOnOff( ) == ShortMessage.NOTE_ON) {
-//					track.add (factory.createNoteOn(event.getStartEndTick ( ) ,
-//							modifiedNote ,
-//							event.getVelocity() ,
-//							event.getChannel())) ;
-//				}
-//				else {
-//					track.add ( factory.createNoteOff ( event.getStartEndTick ( ) , modifiedNote , event.getChannel ( ) ) ) ;
-//				}
-//			}
-//
-//			// Playing the sequence
-//			Sequencer sequencer = MidiSystem.getSequencer ( ) ;
-//			sequencer.open ( );
-//			sequencer.setSequence(sequence) ;
-//			sequencer.start ( ) ;
-//			while (sequencer.isRunning ( ) | sequencer.isOpen ( ) ) {
-//				Thread.sleep (100) ;
-//			}
-//			Thread.sleep(500) ;
-//			sequencer.close() ;
+			//MidiEventFactoryAbstract factoryAbstract = new StandardMidiEventFactoryAbstract ( ) ;
+			MidiEventFactoryAbstractfactoryAbstract = new LegatoMidiEventFactoryAbstract ( ) ;
+			MidiEventFactoryAbstractfactoryAbstract = new StaccatoMidiEventFactoryAbstract ( ) ;
+
+			MidiEventFactory factory = factoryAbstract.createFactory ( ) ;
+
+			// Choose an instrument strategy ( e . g . , Trumpet , Bass Guitar , Piano )
+			InstrumentStrategy instrumentStrategy = new ElectricBassGuitarStrategy ( ) ;
+			instrumentStrategy.applyInstrument ( track , 0 ) ;
+			instrumentStrategy = new TrumpetStrategy ( ) ;
+			instrumentStrategy.applyInstrument (track , 1 ) ;
+
+			// Choose a pitch strategy ( e.g. , HigherPitch ,LowerPitch )
+			PitchStrategy pitchStrategy = new HigherPitchStrategy ( );
+			for (MidiEventData event:midiEvents ) {
+				int modifiedNote = pitchStrategy.modifyPitch ( event.getNote ( ) ) ;
+
+				// call this as much as you want if you want to get a higher pitch
+				modifiedNote = pitchStrategy.modifyPitch (modifiedNote ) ;
+				if (event.getNoteOnOff( ) == ShortMessage.NOTE_ON) {
+					track.add (factory.createNoteOn(event.getStartEndTick ( ) ,
+							modifiedNote ,
+							event.getVelocity() ,
+							event.getChannel())) ;
+				}
+				else {
+					track.add ( factory.createNoteOff ( event.getStartEndTick ( ) , modifiedNote , event.getChannel ( ) ) ) ;
+				}
+			}
+
+			// Playing the sequence
+			Sequencer sequencer = MidiSystem.getSequencer ( ) ;
+			sequencer.open ( );
+			sequencer.setSequence(sequence) ;
+			sequencer.start ( ) ;
+			while (sequencer.isRunning ( ) | sequencer.isOpen ( ) ) {
+				Thread.sleep (100) ;
+			}
+			Thread.sleep(500) ;
+			sequencer.close() ;
 		} catch (Exception e) {
 			e.printStackTrace( );
 		}
